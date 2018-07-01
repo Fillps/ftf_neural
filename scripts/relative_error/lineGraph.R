c1bdfdf5671601bf56be57e829d5fc50b74e182d
Filipe <filipess@live.com>
2018-06-30 23:23:45 -0300

    change graph label to FIT Reduction

--- scripts/relative_error/lineGraph.R
+++ scripts/relative_error/lineGraph.R
@@ -13,7 +13,7 @@ pdf(file = args[2])
 ggplot(dat, aes(x=errLimit, y=percentage, colour=benchmark), method = "lm", formula = y ~ poly(x, 10)) +
 ylim(0,100)+
 geom_line(size=1.1) +
-labs(x = "Tolerated Relative Error [%]", y = "PVF/AVF rate [%]", color="Benchmark") +
+labs(x = "Tolerated Relative Error [%]", y = "FIT Reduction [%]", color="Benchmark") +
 theme(legend.position="bottom",axis.text.x=element_text(size=14) , axis.text.y=element_text(size=14) , text = element_text(size = 14)) 
 
 dev.off()
